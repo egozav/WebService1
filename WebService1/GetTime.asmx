009c380a411361a4a7fac873d1627891432f9c7b
Egor Zavyalov <zed0@mail.ru>
2019-10-14 00:32:13 +0300

    C1

---
+++ WebService1/GetTime.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="GetTime.asmx.cs" Class="WebService1.GetTime" %>
---
+++ WebService1/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebService1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            GetTime webService = new GetTime();
+            List<int> lstIntegers = new List<int> { 5, 6, 7 };
+            Label1.Text = "Output of WebService: " + webService.Add(lstIntegers).ToString();
+
+        }
+    }
+}
